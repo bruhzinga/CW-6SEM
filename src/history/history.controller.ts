3f6ab6b75cb0414133b3fca4d3ce54d017c070e2
DzmitryZ <zvor2003@gmail.com>
2023-04-04 17:34:34 +0300

    added progress

--- src/history/history.controller.ts
+++ src/history/history.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Param, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { HistoryService } from './history.service';
 import { AuthGuard } from '@nestjs/passport';
 
@@ -7,8 +15,20 @@ export class HistoryController {
   constructor(private readonly historyService: HistoryService) {}
 
   @Post(':id')
-  @UseGuards(AuthGuard('jwt'))
-  async create(@Req() req, @Param('id') id: string) {
-    return this.historyService.create(req.user.id, +id);
+  async create(
+    @Req() req,
+    @Body() body: { progress: number },
+    @Param('id') id: string,
+  ) {
+    return this.historyService.create(req.user.id, +id, body.progress);
+  }
+
+  @Get(':movieId')
+  async getHistory(@Req() req, @Param('movieId') movieId: string) {
+    return this.historyService.getHistoryById(req.user.id, +movieId);
+  }
+  @Get()
+  async getHistoryByUser(@Req() req) {
+    return this.historyService.getHistoryByUser(req.user.id);
   }
 }
--- src/history/history.service.ts
+++ src/history/history.service.ts
@@ -5,9 +5,10 @@ import { PrismaService } from '../prisma/prisma.service';
 export class HistoryService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async create(userId: string, movieId: number) {
+  async create(userId: string, movieId: number, progress: number) {
     return this.prisma.history.create({
       data: {
+        progress: progress ? progress : 0,
         Movie: {
           connect: {
             id: movieId,
@@ -16,6 +17,42 @@ export class HistoryService {
         User: {
           connect: {
             id: userId,
+          },
+        },
+      },
+    });
+  }
+
+  getHistoryById(userID, filmId: number) {
+    return this.prisma.history.findMany({
+      orderBy: {
+        time: 'desc',
+      },
+      where: {
+        userId: userID,
+        movieId: filmId,
+      },
+      select: {
+        progress: true,
+      },
+      take: 1,
+    });
+  }
+
+  getHistoryByUser(userId: string) {
+    return this.prisma.history.findMany({
+      where: {
+        userId: userId,
+      },
+      orderBy: {
+        time: 'desc',
+      },
+      select: {
+        time: true,
+        Movie: {
+          select: {
+            id: true,
+            title: true,
           },
         },
       },
