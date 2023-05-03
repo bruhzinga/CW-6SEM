984f48e6b31eb13856728561594af9a1764692ec
DzmitryZ <zvor2003@gmail.com>
2023-05-03 20:43:14 +0300

    slight movie fix

--- src/Movie/Movie.jsx
+++ src/Movie/Movie.jsx
@@ -17,11 +17,11 @@ function Movie(props) {
     useEffect( () => {
         fetchWrapper.get(`${import.meta.env.VITE_API_URL}/movies/${id}`)
             .then( async movie => {
-                console.log(movie)
+                console.log(movie);
                 setMovie(movie);
                 setTrailerIds( movie.Video.filter(item => item.type === 'trailer').map(item => item.id));
                 setImageIds(movie.Image.map(item => item.id));
-                setFilmID(movie.Video.filter(item => item.type === 'movie')[0].id);
+                setFilmID(movie.Video.filter(item => item.type === 'movie')[0]?.id || 1 );
             })
             .catch((reason) => {
                 console.log("Movie with id " + id + " does not exist")
