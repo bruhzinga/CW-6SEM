a91697e05eb109e183bddfd15cd06c087c6fabf7
DzmitryZ <zvor2003@gmail.com>
2023-05-13 13:43:17 +0300

    HTTPS

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,18 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
+import mkcert from 'vite-plugin-mkcert'
 
 const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+    server: { https: true },
+    plugins: [react(), viteTsconfigPaths(), svgrPlugin(),mkcert()],
     resolve:{
         alias:{
             '@' : path.resolve(__dirname, './src')
         },
     },
-    plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
 });
